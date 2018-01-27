3958c7f2bad335331df8ad803bf2c905ee3931bd
David Shaub <davidshaub@gmx.com>
2018-01-27 10:15:03 -0700

    finish

--- hw1/hw1_problem1.py
+++ hw1/hw1_problem1.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
 from multiprocessing.dummy import Pool as ThreadPool
 from random import randint
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_files', type=int,
+                    help='Number of files to write')
+parser.add_argument('--num_lines', type=int,
+                    help='Number of lines to write in each file')
+args = parser.parse_args()
+num_files = args.num_files
+num_lines = args.num_lines
 
 def generate_random():
+	"""
+	Generate a single string containing three random numbers between
+	0 and 10 (inclusive) and separated by a space.
+	"""
 	result = str(randint(0, 10)) + ' ' + str(randint(0, 10)) + ' '
 	result += str(randint(0, 10)) + '\n'
 	return(result)
 
+
 def write_file(file_number, num_lines):
+	"""
+	Write a file with random numbers in the specified format
+	a given number of lines
+	:param file_number: The number of the file--will appear in filename
+	:param num_lines: The number of lines to generate in the file
+	"""
 	file_name = 'david_shaub' + str(file_number) + '.txt'
 	with open(file_name, 'w') as f:
 		for line in range(num_lines):
 			result = generate_random()
 			f.write(result)
 
-numFiles = 3
-numLines = 100
 
-
-worker = [i for i in range(numFiles)]
-pool = ThreadPool(4) 
-results = pool.map(my_function, )
+worker = [i for i in range(num_files)]
+pool = ThreadPool(num_files)
+results = pool.map(lambda x: write_file(file_number=x, num_lines=num_lines),
+                   worker)
