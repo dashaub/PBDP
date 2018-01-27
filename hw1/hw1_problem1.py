83b4fd15f9a3a0cc6b14b7e946dc775d8c5dd0f1
David Shaub <davidshaub@gmx.com>
2018-01-27 09:40:01 -0700

    functions

--- hw1/hw1_problem1.py
+++ hw1/hw1_problem1.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 from multiprocessing.dummy import Pool as ThreadPool
+from random import randint
 
-def format_line(arr):
-	result = ''
-	for num in arr:
-		result +=
+def generate_random():
+	result = str(randint(0, 10)) + ' ' + str(randint(0, 10)) + ' '
+	result += str(randint(0, 10)) + '\n'
+	return(result)
 
 def write_file(file_number, num_lines):
 	file_name = 'david_shaub' + str(file_number) + '.txt'
 	with open(file_name, 'w') as f:
 		for line in range(num_lines):
-			arr = (np.random.rand(3)*11).astype(int)
-			result = str(arr)[1:-1] + '\n'
+			result = generate_random()
 			f.write(result)
 
 numFiles = 3
