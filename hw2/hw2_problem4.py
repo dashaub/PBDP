4dce8227e80bb320a1224f01b7eb748da44003e1
David Shaub <davidshaub@gmx.com>
2018-02-06 20:02:39 -0700

    files

---
+++ hw2/hw2_problem4.py
@@ -0,0 +1,5 @@
+"""
+Load 4 logfiles--each with a thread--and answer 3 queries about the data
+"""
+logfiles = ['log_file_01.txt', 'log_file_02.txt',
+            'log_file_03.txt', 'log_file_04.txt']
