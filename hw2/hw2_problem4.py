a2ee28c6c882089c5ae4c7645cdcb0f9796ff6f0
David Shaub <davidshaub@gmx.com>
2018-02-06 20:40:22 -0700

    process data

--- hw2/hw2_problem4.py
+++ hw2/hw2_problem4.py
@@ -4,6 +4,23 @@ Load 4 logfiles--each with a thread--and answer 3 queries about the data
 logfiles = ['log_file_01.txt', 'log_file_02.txt',
             'log_file_03.txt', 'log_file_04.txt']
 
+shared = {}
+def process_data(timestamp, url user):
+    try:
+        # Test if the URL exists already
+        _ = shared[url]
+        try:
+            # Now test if the subdict exists
+            count = shared[url][user]
+            # Increment the count
+            shared[url][user] += 1
+        except(KeyError) as ke2:
+            # Create the subdict
+        shared[url][user] = 1
+    except(KeyError) as ke1:
+        # URL does not exist, so create it and a subdict with the user
+        shared[url] = {user: 1}
+
 sep = '\t'
 with open('log_file_01.txt', 'r') as f:
     # Read first line
@@ -15,5 +32,6 @@ with open('log_file_01.txt', 'r') as f:
         timestamp = parsed[0]
         url = parsed[1]
         user = parsed[2]
+        process_data(timestamp, url, user)
         # Read next line
         line = f.readline()
