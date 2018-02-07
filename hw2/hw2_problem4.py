d30c93206f23eb3a7cf7abe159f057c3c1be447f
David Shaub <davidshaub@gmx.com>
2018-02-06 20:58:03 -0700

    class

--- hw2/hw2_problem4.py
+++ hw2/hw2_problem4.py
@@ -1,37 +1,66 @@
 """
 Load 4 logfiles--each with a thread--and answer 3 queries about the data
 """
-logfiles = ['log_file_01.txt', 'log_file_02.txt',
-            'log_file_03.txt', 'log_file_04.txt']
 
-shared = {}
-def process_data(timestamp, url user):
-    try:
-        # Test if the URL exists already
-        _ = shared[url]
+SEP = '\t'
+class LogProcessor():
+    def __init__(self, num_files=4):
+        """
+        Constructor
+        :param num_files: The number of files (and threads)
+        """
+        # Lock for the shared state
+        self.lock = threading.Lock()
+        # The shared state, stored as nested dict
+        self.shared = {}
+        # The filenames to process
+        self.files = ['log_file_0' + str(i + 1) + '.txt' for i in range(num_files)]
+
+    def process_data(self, url, user):
+        """
+        Add a single observation to the shared state
+        :param timestamp: The timestamp of the observation
+        :param url: The URL of the observation
+        :param user: The uesr of the observation
+        """
         try:
-            # Now test if the subdict exists
-            count = shared[url][user]
-            # Increment the count
-            shared[url][user] += 1
-        except(KeyError) as ke2:
-            # Create the subdict
-        shared[url][user] = 1
-    except(KeyError) as ke1:
-        # URL does not exist, so create it and a subdict with the user
-        shared[url] = {user: 1}
+            # Test if the URL exists already
+            _ = shared[url]
+            try:
+                # Now test if the subdict exists
+                count = shared[url][user]
+                # Increment the count
+                shared[url][user] = count + 1
+            except(KeyError) as ke2:
+                # User does not exist in subdict, so create it
+                shared[url][user] = 1
+        except(KeyError) as ke1:
+            # URL does not exist, so create it and a subdict with the user
+            shared[url] = {user: 1}
 
-sep = '\t'
-with open('log_file_01.txt', 'r') as f:
-    # Read first line
-    line = f.readline()
-    # Stop reading on EOF
-    while(len(line) > 0):
-        # Split the string into a list with timestamp, URL, and user
-        parsed = line.split(sep)
-        timestamp = parsed[0]
-        url = parsed[1]
-        user = parsed[2]
-        process_data(timestamp, url, user)
-        # Read next line
-        line = f.readline()
+    def process_log(self, filename):
+        """
+        Process a single logfile
+        :param filename: The filename of the log to process
+        """
+        with open(filename, 'r') as f:
+            # Read first line
+            line = f.readline()
+            # Stop reading on EOF
+            while(len(line) > 0):
+                # Split the string into a list with timestamp, URL, and user
+                parsed = line.split(SEP)
+                # Timestamp isn't actually needed
+                timestamp = parsed[0]
+                url = parsed[1]
+                user = parsed[2]
+                process_data(url, user)
+                # Read next line
+                line = f.readline()
+
+    def process_all(self):
+        """
+        Process all the log files.
+        """
+        for filename in self.files:
+            self.process_log(filename)
