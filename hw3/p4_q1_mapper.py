08b2b8f8c2fd134754158f5c73f2c2aa86bfad00
David Shaub <davidshaub@gmx.com>
2018-02-15 10:24:15 -0700

    group by url-hour

--- hw3/p4_q1_mapper.py
+++ hw3/p4_q1_mapper.py
@@ -2,9 +2,19 @@
 
 import sys
 
+def extract_hour(timestamp):
+    """
+    Return a timestamp with no more precision than the hour.
+    :param timestamp: A string representation of a timestamp
+    """
+    return timestamp[:13]
+
 for line in sys.stdin:
     current_line = line.strip().split('\t')
     if current_line is not None:
+        timestamp = current_line[0]
+        hour = extract_hour(timestamp)
         url = current_line[1]
-        # Send all URLs to the same reducer
-        print("1\t{}".format(url))
+        url_hour = url + ' : ' + hour
+        # Key is the url-hour-timestamp, value is count 1
+        print("{}\t1".format(url_hour))
