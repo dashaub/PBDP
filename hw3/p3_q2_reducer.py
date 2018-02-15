5c58985b004a8c4ccba26bdbc5a51fe28d76ef56
David Shaub <davidshaub@gmx.com>
2018-02-14 20:31:43 -0700

    reducer updates

--- hw3/p3_q2_reducer.py
+++ hw3/p3_q2_reducer.py
@@ -4,24 +4,22 @@ import sys
 
 current_user = None
 current_url = None
-count = 0
+count = 1
 for line in sys.stdin:
-    current_line = line.strip().split('\t')
-    if len(current_line) == 2:
-        url = current_line[0]
-        user = current_line[1]
-
+    url, user = current_line = line.strip().split('\t')
+    if current_url:
         # If same URL, we might increment count
         if url == current_url:
             # Only increment if this is a new user
             if user != current_user:
-                current_user = user
                 count += 1
         else:
             # Only emit results if there is data
             if count > 0:
                 print("{}\t{}".format(url, count))
-            current_url = url
-            current_user = user
             count = 1
+    current_user = user
+    current_url = url
 
+if count > 0:
+    print("{}\t{}".format(url, count))
