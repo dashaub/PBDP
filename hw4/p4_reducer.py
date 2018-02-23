2dbb33829decc1bd1c4b01e1b1805687811e1c83
David Shaub <davidshaub@gmx.com>
2018-02-23 10:15:23 -0700

    p5

--- hw4/p4_reducer.py
+++ hw4/p4_reducer.py
@@ -9,7 +9,9 @@ for line in sys.stdin:
     current_line = line.strip().split('\t')
     if current_line is not None and len(current_line) == 2:
         hour, count = current_line
+        # If a new hour is encountered we will reset the count and possibly print a result
         if hour != current_item:
+            # Print after first item
             if current_item is not None:
                 print('{}\t{}'.format(current_item, current_count))
             current_item = hour
---
+++ hw4/p5_avrowriter.py
@@ -0,0 +1,28 @@
+"""Load the text logfiles and save them in a single avro file"""
+import avro.schema
+from avro.datafile import DataFileWriter
+from avro.io import DatumWriter
+import hashlib
+
+# Read the schema file
+schema = avro.schema.parse(open('input_logs_p5.avsc', 'rb').read())
+
+# Define the files to process
+# Intentionallly read each file twice so we have duplicates
+num_files = 4
+input_files = ['logs_{}.txt'.format(i % num_files) for i in range(num_files * 2)]
+with DataFileWriter(open('logs.avro', 'wb'), DatumWriter(), schema) as writer:
+    # Process each input file
+    for input_file in input_files:
+        # Open each input file
+        with open(input_file, 'r') as current_file:
+            # Process each line in each input file
+            for line in current_file:
+                current_line = line.strip().split('\t')
+                # Only parse and write if there is correct input
+                if len(current_line) == 3:
+                    timestamp, url, user = current_line
+                    # Generate uuid for the line
+                    uuid = hashlib.md5sum(current_line).hexdigest()
+                    # Write to avro
+                    writer.append({'uuid': uuid, 'timestamp': timestamp, 'url': url, 'user': user})
