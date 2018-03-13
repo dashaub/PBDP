5b7024b17b4d529537d4d5c40497039e39f68984
David Shaub <davidshaub@gmx.com>
2018-03-13 08:59:22 -0600

    p3

--- hw7/p2_q1.py
+++ hw7/p2_q1.py
@@ -1,12 +1,12 @@
 """
-Get count of unique URLs by hour
+Get count of unique URLs by hour--this time with deduplication
 """
 
 from pyspark import SparkContext, SparkConf
 from datetime import datetime
 
 
-conf = SparkConf().setMaster("local[*]").setAppName("p1_q1")
+conf = SparkConf().setMaster("local[*]").setAppName("p2_q1")
 sc = SparkContext(conf = conf)
 
 def extract_hourpart_url(dat):
@@ -18,12 +18,11 @@ def extract_hourpart_url(dat):
     hourpart = timestamp[:13]
     return '{} {}'.format(hourpart, url)
 
-logs = sc.textFile('/Users/david.shaub/PBDP/hw7/hw7_logs*.txt')
+logs = sc.textFile('/Users/david.shaub/PBDP/hw7/hw7_logs*.txt').distinct()
 # Convert full timestamp to only hour and return only needed fields
 hour_url = logs.map(extract_hourpart_url)
 # Form key/values, and get grouped counts
-# Note: duplicates are present here but will be allowed for this problem
 tuples = hour_url.map(lambda x: tuple([x.split(' ')[0], 1]))
 q1 = tuples.reduceByKey(lambda x, y: x + y)
 
-q1.coalesce(1).saveAsTextFile("output_p1_q1/")
+q1.coalesce(1).saveAsTextFile("output_p2_q1/")
