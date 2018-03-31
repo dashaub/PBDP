d28b0bcc89d513cbb118d00801e34b18964d4366
David Shaub <davidshaub@gmx.com>
2018-03-30 22:43:40 -0600

    unique url

---
+++ hw8/unique_url.py
@@ -0,0 +1,25 @@
+"""
+Get unique URLs by hour
+"""
+
+from pyspark import SparkContext, SparkConf
+from datetime import datetime
+
+
+conf = SparkConf().setAppName("unique_url")
+sc = SparkContext(conf = conf)
+
+def extract_hourpart_url(dat):
+    """
+    Return timestamp (up to the hour) and the URL
+    :param dat: An RDD from our log data
+    """
+    _, timestamp, url, _ = dat.split(' ')
+    hourpart = timestamp[:13] + ':00:00'
+    return '{} {}'.format(hourpart, url)
+
+logs = sc.textFile('hw7_logs*.txt')
+# Convert full timestamp to only hour and return only needed fields and remove duplicate URLs
+hour_url = logs.map(extract_hourpart_url).distinct()
+
+hour_url.coalesce(1).saveAsTextFile("output_unique_url/")
